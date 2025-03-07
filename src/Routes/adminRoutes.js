0c3021548eb56196981bb44ac0658f4b53bda692
Maria Francisca Mellado <136738816+MelladoDev@users.noreply.github.com>
2025-03-07 18:49:07 -0300

    Update adminRoutes.js

--- src/Routes/adminRoutes.js
+++ src/Routes/adminRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Ruta para que un admin se autentique (login) y obtenga su información
 // Se espera que se envíen en los headers las credenciales (usuario y contraseña)
 router.get("/me", verifyAdmin, AdminController.getAdmin);
-
+router.get("/",  AdminController.getAdmin);
 // Ruta para crear un nuevo administrador (convertir un usuario en admin)
 // Solo se podrá acceder si el que realiza la petición tiene credenciales de admin válidas
 router.post("/create",  AdminController.createAdmin);
