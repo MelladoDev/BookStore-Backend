d6febe2f43ba0046df1fa24fa028fc39b0b27629
Maria Francisca Mellado <136738816+MelladoDev@users.noreply.github.com>
2025-03-09 20:49:17 -0300

    Update productModel.js

--- src/Models/productModel.js
+++ src/Models/productModel.js
@@ -2,7 +2,7 @@ import pool from "../DataBase/config.js";
 
 export const getAllProducts = async () => {
   const result = await pool.query(`
-        SELECT id_producto, nombre, descripcion, precio, stock, imagen, id_categoria 
+        SELECT id_producto, nombre, autor, descripcion, precio, stock, imagen, id_categoria 
         FROM productos 
         ORDER BY id_producto ASC
     `);
@@ -19,6 +19,7 @@ export const getProductById = async (id) => {
 
 export const createProduct = async (
   nombre,
+  autor,
   descripcion,
   precio,
   stock,
@@ -26,8 +27,8 @@ export const createProduct = async (
   id_categoria
 ) => {
   const result = await pool.query(
-    "INSERT INTO productos (nombre, descripcion, precio, stock, imagen, id_categoria, fecha_adicion) VALUES ($1, $2, $3, $4, $5, $6, CURRENT_DATE) RETURNING *",
-    [nombre, descripcion, precio, stock, imagen, id_categoria]
+    "INSERT INTO productos (nombre, autor, descripcion, precio, stock, imagen, id_categoria, fecha_adicion) VALUES ($1, $2, $3, $4, $5, $6, $7 CURRENT_DATE) RETURNING *",
+    [nombre, autor, descripcion, precio, stock, imagen, id_categoria]
   );
   return result.rows[0];
 };
@@ -35,6 +36,7 @@ export const createProduct = async (
 export const updateProduct = async (
   id,
   nombre,
+  autor,
   descripcion,
   precio,
   stock,
@@ -42,8 +44,8 @@ export const updateProduct = async (
   id_categoria
 ) => {
   const result = await pool.query(
-    "UPDATE productos SET nombre = $1, descripcion = $2, precio = $3, stock = $4, imagen = $5, id_categoria = $6 WHERE id_producto = $7 RETURNING *",
-    [nombre, descripcion, precio, stock, imagen, id_categoria, id]
+    "UPDATE productos SET nombre = $1, autor = $2, descripcion = $3, precio = $4, stock = $5, imagen = $6, id_categoria = $7 WHERE id_producto = $8 RETURNING *",
+    [nombre, autor, descripcion, precio, stock, imagen, id_categoria, id]
   );
   return result.rows[0];
 };
